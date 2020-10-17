2db3b5e4dd48f5c2d095028cb3736b0d660df9cc
brunneis <dev@brunneis.com>
2020-10-17 17:05:12 +0000

    Update Docker build script

--- docker/build.sh
+++ docker/build.sh
@@ -4,6 +4,6 @@ source env.sh
 docker rmi catenae/link-stopover:$VERSION 2> /dev/null
 tar cf ../../catenae.tar ../
 mv ../../catenae.tar .
-docker build -t catenae/link-stopover:$VERSION .
+docker build --no-cache -t catenae/link-stopover:$VERSION .
 docker tag catenae/link-stopover:$VERSION catenae/link-stopover:latest
 rm -f catenae.tar
